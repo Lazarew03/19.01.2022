32ca11d84e214199d5375554eb5c94a97af65add
user <user@C1011>
2022-01-19 14:27:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Phone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Phone
+    {
+        private string _memory;
+        private string _model;
+        private bool _old;
+       
+
+       
+        
+
+        public string Memory { get => _memory; set => _memory = value; }
+        public string Model { get => _model; set => _model = value; }
+        public bool Old { get => _old; set => _old = value; }
+        
+        
+
+        public override string ToString()
+        {
+            return Model + " " + Old + " " + Memory;
+        }
+        
+    }
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        Random random = new Random();
+      
+        Phone[] phones = new Phone[10];
+            for(int i = 0; i < phones.Length; i++)
+
+            {
+                phones[i] = new Phone();    
+
+                string[] models = { "Apple", "POCO", "SAMSUNG", "SONY" };
+                bool[] vs = { true, false };
+                string[] memory = { "64 Гб", "128 Гб", "256 Гб", "512 Гб" };
+
+                int index = random.Next(models.Length);
+                int index2 = random.Next(vs.Length);
+                int index3 = random.Next(memory.Length);
+
+                phones[i].Model = models[index];
+                phones[i].Old = vs[index2];
+                phones[i].Memory = memory[index3];
+
+            }
+            for (int i = 0; i < phones.Length; i++)
+            {
+                Console.WriteLine(phones[i]);
+               
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+
+        
+        
+    }
+
+}
